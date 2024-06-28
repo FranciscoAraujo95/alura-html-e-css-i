d85c9def5e84b990e4ffc183271b94501fc4f932
Francisco <fhydglan@gmail.com>
2024-06-27 23:39:51 -0300

    I made some cool stuff

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+let imagemPerfil = document.querySelector(".apresentacao__imagem");
 let btnCafe = document
   .querySelector("#cabecalho__menu__link__cafe")
   .addEventListener("click", () => {
+    imagemPerfil.setAttribute("src", "./img/perfil-chico-figma.png");
     document.body.classList.remove("active__forest");
     document.body.classList.remove("active__tsunami");
   });
@@ -8,6 +10,7 @@ let btnCafe = document
 let btnForest = document
   .querySelector("#cabecalho__menu__link__forest")
   .addEventListener("click", () => {
+    imagemPerfil.setAttribute("src", "./img/perfil-chico-figma-forest.png");
     document.body.classList.remove("active__tsunami");
     document.body.classList.add("active__forest");
   });
@@ -15,6 +18,7 @@ let btnForest = document
 let btnTsunami = document
   .querySelector("#cabecalho__menu__link__tsunami")
   .addEventListener("click", () => {
+    imagemPerfil.setAttribute("src", "./img/perfil-chico-figma-tsunami.png");
     document.body.classList.remove("active__forest");
     document.body.classList.add("active__tsunami");
   });
